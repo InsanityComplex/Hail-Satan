8638576c73739d9fb273b9dbd8a1a4f2d747790b
Jessica Coan <jcoan@email.arizona.edu>
2018-10-27 16:20:38 -0700

    updated manifest

---
+++ Assets/BirdArmController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdArmController : MonoBehaviour {
+	
+	void Start () {
+        Cursor.lockState = CursorLockMode.None;
+	}
+	
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+	}
+}
