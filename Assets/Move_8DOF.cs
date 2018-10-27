986990df927aaa535572677b7f825bd1ceeddb35
InsanityComplex <ldog278@gmail.com>
2018-10-27 01:31:12 -0700

    Added simple player

---
+++ Assets/Move_8DOF.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Move_8DOF : MonoBehaviour {
+
+    public float speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        //really bad code but whatever
+
+        Vector3 loc = transform.position;
+
+        loc.z += speed * Time.deltaTime;
+
+    }
+}
