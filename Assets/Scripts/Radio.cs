24098c503c802d8dc5620348e2b80f6f57555897
Jessica Coan <jcoan188@gmail.com>
2018-10-27 16:52:13 -0700

    radio with sound

---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Radio : MonoBehaviour {
+
+    private bool clickable = false;
+    private bool MouseOnObject = false;
+
+    private bool on = false;
+
+
+    // Use this for initialization
+    void Start(){
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            clickable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            clickable = false;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        MouseOnObject = true;
+        Debug.Log("Mouse On");
+    }
+
+    void OnMouseExit()
+    {
+        MouseOnObject = false;
+        Debug.Log("Mouse Off");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) && clickable)
+        {
+            if (!on)
+            {
+                this.GetComponent<AudioSource>().Play();
+                on = true;
+            }
+
+            else
+            {
+                this.GetComponent<AudioSource>().Stop();
+                on = false;
+            }
+        }
+
+    }
+}
