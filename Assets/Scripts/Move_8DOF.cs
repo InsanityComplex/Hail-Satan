a391d7c87a1e47a703bfded9c66da0162ed60ba0
Jessica Coan <jcoan@email.arizona.edu>
2018-10-27 12:10:58 -0700

    project organization, importing my dialogue manager

---
+++ Assets/Scripts/Move_8DOF.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Move_8DOF : MonoBehaviour {
+
+    public float speed = 20.0f;
+
+    CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        //really bad code but whatever
+
+        Vector3 heading = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * speed;
+        heading += Physics.gravity * Time.deltaTime;
+        controller.Move(heading * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/TriggerDialogue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogue : MonoBehaviour {
+
+    public Dialogue[] Words;
+
+    Queue<Dialogue> diaQueue;
+   
+
+    public void Start()
+    {
+        diaQueue = new Queue<Dialogue>();
+        for (int i = 0; i < Words.Length; i++)
+        {
+            diaQueue.Enqueue(Words[i]);
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject.Find("Dialogue Manager").GetComponent<DialogueManager>().StartDialogue(diaQueue);
+            Debug.Log("Allowing interaction");
+
+        }
+    }
+}
