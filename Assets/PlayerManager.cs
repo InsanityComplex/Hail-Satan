0c54d0d8ba95d21633172f70d3134c4e840237da
Jessica Coan <jcoan188@gmail.com>
2018-10-28 01:00:47 -0700

    more bug fixes

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public bool[] Completition;
+
+    int current;
+
+    void UpdateCompletition()
+    {
+        Completition[current] = true;
+        current++;
+    }
+
+    public bool IsComplete()
+    {
+        bool output = true;
+        for (int i = 0; i < Completition.Length; i++)
+        {
+            output = output && Completition[i];
+        }
+        return output;
+    }
+}
